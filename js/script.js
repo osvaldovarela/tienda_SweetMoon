53c170e4e4f2cb13617daa0942eb9638f8efe04b
Nati <nataliaforte@gmail.com>
2024-05-09 11:26:21 -0300

    validacion js nombre formulario

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
-function saludar() {
-  console.log("Hola");
-}
-saludar();
+
+
+const form = document.querySelector(".form");
+const nombre = document.querySelector("#nombre");
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = nombre.value.trim();
+
+    if(title.lenght >=3 ){
+      const task = {
+        
+        title,
+        complete: false,
+      };
+    }else {
+        const error = document.querySelector(".error");
+        error.textContent = "Debe ingresar 3 caracteres o mas";
+
+        setTimeout(() => {
+          error.textContent = "";
+        }, 2000);
+    
+    };
+   
+  
+});
