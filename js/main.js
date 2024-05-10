70b86fb082f30667e7c891687d80bf81bb769afd
alizunega <alizunega84@gmail.com>
2024-05-10 11:55:17 -0300

    funcionalidad add al carrito

--- js/main.js
+++ js/main.js
@@ -35,20 +35,21 @@ function irArriba() {
 // funcion para mostrar el modal al presionar el boton añadir
 //lista de todos los botones para añadir
 const btns_add_item = document.querySelectorAll(".add-product");
-
+const contadorSpan = document.querySelector("#contador");
 const closeModal = document.querySelector("#btn-cerrar-modal");
 
 // Iteramos sobre cada botón y agregamos el event listener
+let contador = 0;
 btns_add_item.forEach((btn) => {
   btn.addEventListener("click", () => {
     window.modal.show();
+    contador++;
+
+    // Actualiza el contenido del contador en la página
+    contadorSpan.textContent = contador;
   });
 });
 
 closeModal.addEventListener("click", () => {
   window.modal.close();
 });
-
-
-
-
