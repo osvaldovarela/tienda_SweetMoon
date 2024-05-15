25cc761fe4d07bfa1a089a33a0b8d95d81c996f0
alizunega <alizunega84@gmail.com>
2024-05-15 11:30:03 -0300

    creacion header dinamico

--- js/carrito.js
+++ js/carrito.js
@@ -1,2 +1,65 @@
-// aqui planteare las funciones para mostrar los productos seleccionados y
-// vaciar el carrito
+// // aqui planteare las funciones para mostrar los productos seleccionados y
+// // vaciar el carrito
+// // Crea elementos HTML
+// const topNav = document.createElement("nav");
+// topNav.classList.add("topNav");
+// topNav.setAttribute("id", "myTopnav");
+
+// const ulNav = document.createElement("ul");
+// const logoLi = document.createElement("li");
+// const logoImg = document.createElement("img");
+// const nombrePaginaLi = document.createElement("li");
+// const nombrePaginaH1 = document.createElement("h1");
+// const menuLi = document.createElement("li");
+// const menuUl = document.createElement("ul");
+// const carritoLi = document.createElement("li");
+// const aCarrito = document.createElement("a");
+
+// const carrito = document.createElement("i");
+// carrito.classList.add("icon");
+// carrito.classList.add("fa");
+// carrito.classList.add("fa-shopping-cart");
+
+// carrito.setAttribute("aria-hidden", true);
+// carrito.setAttribute("tittle", "Carrito");
+
+// aCarrito.textContent = "Pedido";
+// aCarrito.setAttribute("id", "pedido");
+
+// aCarrito.appendChild(carrito);
+// carritoLi.appendChild(aCarrito);
+
+// logoImg.setAttribute("src", "./img/logo/logo.png");
+// logoImg.setAttribute("alt", "Logo SweetMoon");
+// logoImg.classList.add("logo");
+// logoImg.classList.add("active");
+
+// logoLi.appendChild(logoImg);
+
+// nombrePaginaH1.textContent = "Sweet Moon";
+// nombrePaginaH1.classList.add("active");
+// nombrePaginaLi.appendChild(nombrePaginaH1);
+
+// secciones.forEach((seccion) => {
+//   let li = document.createElement("li");
+//   let a = document.createElement("a");
+
+//   a.setAttribute("href", seccion.href);
+//   li.classList.add("seccionItem");
+//   a.textContent = seccion.nombre;
+//   li.appendChild(a);
+//   menuUl.appendChild(li);
+// });
+
+// menuUl.appendChild(carritoLi);
+// menuUl.classList.add("menu-items");
+// menuLi.appendChild(menuUl);
+
+// ulNav.appendChild(logoLi);
+// ulNav.appendChild(nombrePaginaLi);
+
+// ulNav.appendChild(menuLi);
+
+// topNav.appendChild(ulNav);
+
+// header.appendChild(topNav);
---
+++ js/elements.js
@@ -0,0 +1,85 @@
+//evento escucha cuando cargue la pagina
+
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+  //   const footer = document.querySelector("footer");
+
+  //lista de secciones del navegador
+  const secciones = [
+    {
+      nombre: "Home",
+      href: "./index.html",
+    },
+    {
+      nombre: "Sobre Sweet",
+      href: "./aboutme.html",
+    },
+    {
+      nombre: "Productos",
+      href: "./productos.html",
+    },
+    {
+      nombre: "Contacto",
+      href: "./Contacto.html",
+    },
+  ];
+
+  //creacion de las constantes de elementos
+  //elemento imagen
+  const imgLogo = document.createElement("img");
+  imgLogo.classList.add("logo");
+  imgLogo.setAttribute("src", "./img/logo/logo.png");
+  imgLogo.setAttribute("alt", "Logo SweetMoon");
+
+  //elemento nombre
+  const nombrePagina = document.createElement("h1");
+  nombrePagina.classList.add("nombre");
+  nombrePagina.textContent = "Sweet Moon";
+
+  const nav = document.createElement("nav");
+  nav.classList.add("csMenu");
+
+  const inputButton = document.createElement("input");
+  inputButton.setAttribute("id", "hMenuBtn");
+  inputButton.setAttribute("type", "checkbox");
+
+  const labelMenu = document.createElement("label");
+  labelMenu.setAttribute("for", "hMenuBtn");
+
+  const ulMenu = document.createElement("ul");
+  ulMenu.setAttribute("id", "hMenu");
+  ulMenu.classList.add("mVerti");
+
+  const carritoLi = document.createElement("li");
+  const aCarrito = document.createElement("a");
+  const imgCarrito = document.createElement("img");
+
+  imgCarrito.classList.add("carrito");
+  imgCarrito.setAttribute("src", "./img/productos/carrito/carrito.ico");
+
+  aCarrito.textContent = "Pedido";
+  aCarrito.setAttribute("id", "pedido");
+
+  aCarrito.appendChild(imgCarrito);
+  carritoLi.appendChild(aCarrito);
+
+  secciones.forEach((seccion) => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+
+    a.setAttribute("href", seccion.href);
+    a.textContent = seccion.nombre;
+    li.appendChild(a);
+    ulMenu.appendChild(li);
+  });
+
+  ulMenu.appendChild(carritoLi);
+
+  nav.appendChild(inputButton);
+  nav.appendChild(labelMenu);
+  nav.appendChild(ulMenu);
+
+  header.appendChild(imgLogo);
+  header.appendChild(nombrePagina);
+  header.appendChild(nav);
+});
