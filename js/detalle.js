94a3919eac5ddc0a0804e1f10c1a33220e40eebe
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2024-06-10 00:09:59 -0300

    update pagina detalles

---
+++ js/detalle.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Obtener el ID del producto de los parámetros de la URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = parseInt(urlParams.get("id"));
+
+  // URL del archivo JSON local
+  const jsonUrl = "./productos.json";
+
+  fetch(jsonUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("JSON loaded:", data);
+
+      if (!Array.isArray(data.productos)) {
+        throw new Error("Expected an array");
+      }
+
+      // Encontrar el producto correspondiente
+      const articulo = data.productos.find((articulo) => articulo.id === id);
+
+      if (articulo) {
+        const cardContainer = document.getElementById("card-container");
+        cardContainer.innerHTML = `
+                            <div class="card">
+                                <h2>${articulo.nombre || articulo.title}</h2>
+                                <img src="${articulo.source}" alt="${
+          articulo.nombre || articulo.title
+        }" style="width: 100%;">
+                                <p>Precio: ${articulo.precio}</p>
+                            </div>
+                        `;
+      } else {
+        console.error("Artículo no encontrado");
+      }
+    })
+    .catch((error) => {
+      console.error("Error al cargar el JSON:", error);
+    });
+});
